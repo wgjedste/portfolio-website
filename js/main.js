98941b155eeef898a216ec984d6945a62e70d453
Will Gjedsted <wgjedste@gmail.com>
2022-02-02 10:55:45 -0800

    added new var

--- js/main.js
+++ js/main.js
@@ -1,55 +1,67 @@
-const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle')
-    closeMenu = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle");
+closeMenu = document.getElementById("nav-close");
 
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
 
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
+const navLink = document.querySelectorAll(".nav__link");
 
-
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    navMenu.classList.remove('show')
+function linkAction() {
+  navMenu.classList.remove("show");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
-
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
 }
 
+(function () {
+  window.onscroll = function () {
+    ProgressBar();
+  };
 
-(function(){
-	window.onscroll = function() {ProgressBar()};
+  function ProgressBar() {
+    var winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    var height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.querySelector(".progress-bar").style.width = scrolled + "%";
+  }
+})();
 
-	function ProgressBar() {
-		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-		var scrolled = (winScroll / height) * 100;
-		document.querySelector(".progress-bar").style.width = scrolled + "%";
-	}
-}());
+
+const modalBtn = document.getElementById(".work__title");
+const modalBg = document.getElementById(".work__title");
+
+modalBtn.addEventListener('click', function(){
+    
+})
